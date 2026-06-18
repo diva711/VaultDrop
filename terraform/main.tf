7b67584ac7f65803b4e669dc268d89ca17efc631
Diva Chandra <divachandra583@gmail.com>
2026-06-18 16:08:34 +0530

    feat: add S3 remote backend for shared terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,12 @@ terraform {
     }
   }
   required_version = ">=1.6"
+
+  backend "s3" {
+    bucket = "vaultdrop-tfstate-ap-south-1"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
 }
 
 provider "aws" {
